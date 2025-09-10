8a465984722f40a74371d851d693b43b0e20f196
MunibShafi-aston <230057117@aston.ac.uk>
2025-09-10 16:37:19 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 export default function Home() {
   return <div className="flex">React Practice</div>;
-  <div className="flex">Hi</div>
+  return <div className="flex">React Practice</div>;
+  return <div className="flex">React Practice</div>;
+  return <div className="flex">React Practice</div>;
+
 }
